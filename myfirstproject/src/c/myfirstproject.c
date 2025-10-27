2c1f44311fa01ee7c030d28c9601f92e018acef4
jwuerz <jwuerz@gmail.com>
2025-10-27 13:08:46 +0000

    modified:   myfirstproject/src/c/myfirstproject.c

--- myfirstproject/src/c/myfirstproject.c
+++ myfirstproject/src/c/myfirstproject.c
@@ -3,19 +3,22 @@
 static Window *s_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_ampm_layer;
+static TextLayer *s_ampm_layer2;
 static Layer *s_border_layer;
 
 static void prv_border_draw(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(layer);
   
-  // Draw outer border
+  // Draw outer border (top and bottom only)
   graphics_context_set_stroke_color(ctx, GColorBlack);
   graphics_context_set_stroke_width(ctx, 2);
-  graphics_draw_rect(ctx, GRect(2, 2, bounds.size.w - 4, bounds.size.h - 4));
+  graphics_draw_line(ctx, GPoint(2, 2), GPoint(bounds.size.w - 3, 2)); // Top
+  graphics_draw_line(ctx, GPoint(2, bounds.size.h - 3), GPoint(bounds.size.w - 3, bounds.size.h - 3)); // Bottom
   
-  // Draw inner border for book-like effect
+  // Draw inner border for book-like effect (top and bottom only)
   graphics_context_set_stroke_width(ctx, 1);
-  graphics_draw_rect(ctx, GRect(6, 6, bounds.size.w - 12, bounds.size.h - 12));
+  graphics_draw_line(ctx, GPoint(6, 6), GPoint(bounds.size.w - 7, 6)); // Top
+  graphics_draw_line(ctx, GPoint(6, bounds.size.h - 7), GPoint(bounds.size.w - 7, bounds.size.h - 7)); // Bottom
   
   // Add corner decorations (simple lines)
   graphics_draw_line(ctx, GPoint(2, 10), GPoint(10, 2));
@@ -41,9 +44,16 @@ static void prv_update_time() {
   if (!clock_is_24h_style()) {
     static char ampm_buffer[3];
     strftime(ampm_buffer, sizeof(ampm_buffer), "%p", tick_time);
-    text_layer_set_text(s_ampm_layer, ampm_buffer);
+    if (strcmp(ampm_buffer, "AM") == 0) {
+      text_layer_set_text(s_ampm_layer, "A");
+      text_layer_set_text(s_ampm_layer2, "M");
+    } else {
+      text_layer_set_text(s_ampm_layer, "P");
+      text_layer_set_text(s_ampm_layer2, "M");
+    }
   } else {
     text_layer_set_text(s_ampm_layer, "");
+    text_layer_set_text(s_ampm_layer2, "");
   }
 }
 
@@ -62,31 +72,41 @@ static void prv_window_load(Window *window) {
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
-      GRect(10, 15, bounds.size.w - 50, 50));
+      GRect(5, 10, bounds.size.w - 25, 50));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_36_BOLD_NUMBERS));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentLeft);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
-  // Create the TextLayer for AM/PM
-  s_ampm_layer = text_layer_create(GRect(bounds.size.w - 40, 15, 30, 50));
+  // Create the TextLayer for AM/PM (first letter)
+  s_ampm_layer = text_layer_create(GRect(bounds.size.w - 20, 18, 15, 25));
   text_layer_set_background_color(s_ampm_layer, GColorClear);
   text_layer_set_text_color(s_ampm_layer, GColorBlack);
   text_layer_set_text(s_ampm_layer, "");
-  text_layer_set_font(s_ampm_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-  text_layer_set_text_alignment(s_ampm_layer, GTextAlignmentLeft);
+  text_layer_set_font(s_ampm_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(s_ampm_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_ampm_layer));
+
+  // Create the TextLayer for AM/PM (second letter)
+  s_ampm_layer2 = text_layer_create(GRect(bounds.size.w - 20, 38, 15, 25));
+  text_layer_set_background_color(s_ampm_layer2, GColorClear);
+  text_layer_set_text_color(s_ampm_layer2, GColorBlack);
+  text_layer_set_text(s_ampm_layer2, "");
+  text_layer_set_font(s_ampm_layer2, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(s_ampm_layer2, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_ampm_layer2));
 }
 
 static void prv_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_ampm_layer);
+  text_layer_destroy(s_ampm_layer2);
   layer_destroy(s_border_layer);
 }
 
